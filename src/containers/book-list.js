fd3a21224849649285881af6206a862478760e6e
Tin Chu <mchu.tinho@gmail.com>
2017-11-06 08:47:36 -0600

    book list and book detail

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { selectBook } from "../actions/index";
+import { bindActionCreators } from 'redux';
 
 class BookList extends Component {
 
 	renderList() {
 		return this.props.books.map( (book) => {
 			return(
-				<li key={ book.title } classname="list-group-item">{ book.title }</li>
+				<li
+					key={ book.title }
+					onClick={ () => this.props.selectBook( book ) } //aqui es donde le pasas la dat al state redux
+					className="list-group-item">
+					{ book.title }
+				</li>
 			)
 		});
 	}
@@ -28,5 +35,14 @@ function mapStateToProps(state) {
 	};
 }
 
-// this line produces a Container
-export default connect(mapStateToProps)(BookList);
+// anything returned from this function will end up as props
+// on the BookList container
+function mapDispatchToProps( dispatch ) {
+	// whenever selectBook is called, the result should be passed
+	// to all of our reducers
+	return bindActionCreators({ selectBook: selectBook }, dispatch );
+}
+
+// Promote BookList from a componet to a Container - it needs to know
+// about this new dispatch method, selectBook. Make it available as prop
+export default connect( mapStateToProps, mapDispatchToProps )(BookList);
--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
 	return [
-		{ title: 'Javascript: The Good Parts' },
-		{ title: 'Javascript: The Good Parts 2' },
-		{ title: 'Javascript: The Good Parts 3' },
-		{ title: 'Javascript: The Good Parts 4' }
+		{ title: 'Javascript: The Good Parts', pages: 101 },
+		{ title: 'Javascript: The Good Parts 2', pages: 1 },
+		{ title: 'Javascript: The Good Parts 3', pages: 10 },
+		{ title: 'Javascript: The Good Parts 4', pages: 400 }
 	]
 }
